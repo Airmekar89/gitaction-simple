f232aed964558c0755db4fad6c1e7a80c1d2ca98
Emeka <EIwenofu.89@gmail.com>
2023-09-25 02:16:36 -0400

    fix: upadated code for plans to run in series and to run terraform Contributing to: https://kennedymakhanu1.atlassian.net/browse/<JIRA-TICKET>
    Problem
    
    Solution
    
    Note

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  
+}
+resource "aws_vpc" "myvpc" {
+  cidr_block           = "172.30.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    "Name" = "myvpc"
+    "Env"  = "Dev"
+  }
+}
